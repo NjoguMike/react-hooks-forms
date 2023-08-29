8b457e1a622904084e6b631f7449f29eb8a9de6b
Njogu Michael <michaelfebzen.mb@gmail.com>
2023-08-30 00:30:39 +0300

    Complete Controlled Forms

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,10 +4,20 @@ function Form() {
   const [firstName, setFirstName] = useState("John");
   const [lastName, setLastName] = useState("Henry");
 
+  function handleFirstName (event){
+    const newName = event.target.value
+    console.log(newName)
+    setFirstName(newName)
+  }
+  function handleLastName(event){
+    const newName = event.target.value
+    setLastName(newName)
+  }
+
   return (
     <form>
-      <input type="text" value={firstName} />
-      <input type="text" value={lastName} />
+      <input type="text" value={firstName} onChange={handleFirstName}/>
+      <input type="text" value={lastName} onChange={handleLastName}/>
       <button type="submit">Submit</button>
     </form>
   );
